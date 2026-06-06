6764dcc0ddacecaa7be1b4b58d0edad130013510
PetarBonchev <93945851+PetarBonchev@users.noreply.github.com>
2026-06-06 23:34:31 +0300

    Add initial models

---
+++ backend/Models/Comment.php
@@ -0,0 +1,45 @@
+<?php
+
+class Comment extends Model
+{
+    protected string $table = 'comments';
+
+    public function create(array $data): int
+    {
+        $stmt = $this->db->prepare("
+            INSERT INTO comments
+            (
+                room_item_id,
+                user_id,
+                visibility,
+                content
+            )
+            VALUES (?, ?, ?, ?)
+        ");
+
+        $stmt->execute([
+            $data['room_item_id'],
+            $data['user_id'],
+            $data['visibility'],
+            $data['content']
+        ]);
+
+        return (int)$this->db->lastInsertId();
+    }
+
+    public function getForRoomItem(int $roomItemId): array
+    {
+        $stmt = $this->db->prepare("
+            SELECT c.*, u.first_name, u.last_name
+            FROM comments c
+            JOIN users u
+                ON u.id = c.user_id
+            WHERE c.room_item_id = ?
+            ORDER BY c.created_at
+        ");
+
+        $stmt->execute([$roomItemId]);
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
